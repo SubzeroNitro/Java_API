cd05ac3013aec3ea7d3ae116fb986c74a5ed3ac5
David <drvillase@gmail.com>
2023-10-16 02:44:11 -0400

    Updated the http request parser

--- app/src/main/java/com/subzeronitro/java_api/utilities/http/HttpRequest.java
+++ app/src/main/java/com/subzeronitro/java_api/utilities/http/HttpRequest.java
@@ -1,6 +1,5 @@
 package com.subzeronitro.java_api.utilities.http;
 
-import java.util.stream.Stream;
 import java.util.zip.DataFormatException;
 
 public class HttpRequest {
@@ -21,10 +20,42 @@ public class HttpRequest {
 		String[] lines = header.split("\n");
 		String[] requestInfo = lines[0].split(" ");
 		
+		if (requestInfo.length != 3) {
+			throw new DataFormatException("Invalid HTTP header");
+		}
+		
 		this.header.method = HttpMethod.fromString(requestInfo[0]);
 		
 		if (this.header.method == HttpMethod.INVALID) {
 			throw new DataFormatException("Invalid HTTP method");
 		}
+		
+		String[] uriSections = requestInfo[1].split("\\?|#", 1);
+		
+		this.header.uri.path = uriSections[0];
+		
+		if (uriSections.length == 2) {
+			String[] sectionsAfterPath = uriSections[1].split("#", 1);
+			String[] parameters = sectionsAfterPath[0].split("&");
+
+			for (String parameter : parameters) {
+				String[] pair = parameter.split("=", 1);
+				
+				if (pair.length != 2) {
+					throw new DataFormatException("Invalid parameter: " + parameter);
+				}
+				
+				if (this.header.uri.parameters.containsKey(pair[0])) {
+					throw new DataFormatException("Repeated parameter in URI");
+				}
+				
+				this.header.uri.parameters.put(pair[0], pair[1]);
+			}
+			
+			if (sectionsAfterPath.length == 2) {
+				this.header.uri.fragment = sectionsAfterPath[1];
+			}
+		}
+	
 	}
 }
